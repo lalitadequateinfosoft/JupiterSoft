4fe79b52c08b56307fac606f10929a8e3b9a26be
Lalit Yadav <lalit.yadav@adequateinfosoft.com>
2022-03-09 20:31:55 +0530

    Drag and Drop Event

--- JupiterSoft/JupiterSoft/Pages/CreateTemplate.xaml.cs
+++ JupiterSoft/JupiterSoft/Pages/CreateTemplate.xaml.cs
@@ -20,9 +20,250 @@ namespace JupiterSoft.Pages
     /// </summary>
     public partial class CreateTemplate : Page
     {
+        BrushConverter bc;
         public CreateTemplate()
         {
+            bc = new BrushConverter();
             InitializeComponent();
+        }
+
+        private void ButtonGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var sArea = sender as StackPanel;
+            if(sArea.Name== "ButtonGridArea1")
+            {
+                buttonGrid1.Opacity = 0.5;
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if(sArea.Name== "ButtonGridArea2")
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.Opacity = 0.5;
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (sArea.Name == "ButtonGridArea3")
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.Opacity = 0.5;
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (sArea.Name == "ButtonGridArea4")
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.Opacity = 0.5;
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (sArea.Name == "ButtonGridArea5")
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.Opacity = 0.5;
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (sArea.Name == "ButtonGridArea6")
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.Opacity = 0.5;
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (sArea.Name == "ButtonGridArea7")
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.Opacity = 0.5;
+            }
+            else
+            {
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+        }
+
+        private void Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton==MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(MoveButton, MoveButton, DragDropEffects.Copy);
+            }
+        }
+
+        private void ButtonGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            buttonGrid1.ClearValue(UIElement.OpacityProperty);
+            buttonGrid2.ClearValue(UIElement.OpacityProperty);
+            buttonGrid3.ClearValue(UIElement.OpacityProperty);
+            buttonGrid4.ClearValue(UIElement.OpacityProperty);
+            buttonGrid5.ClearValue(UIElement.OpacityProperty);
+            buttonGrid6.ClearValue(UIElement.OpacityProperty);
+            buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            
+        }
+
+        private void Border_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var cSender=sender as Border;
+            UIElement element = VisualTreeHelper.GetParent(cSender) as UIElement;
+            string pName = (element as Grid).Name;
+            element.Opacity = 0.5;
+            if (pName== "buttonGrid1")
+            {
+                ButtonGridArea1.BringIntoView();
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if(pName== "buttonGrid2")
+            {
+                ButtonGridArea2.BringIntoView();
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (pName == "buttonGrid3")
+            {
+                ButtonGridArea3.BringIntoView();
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                //buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (pName == "buttonGrid4")
+            {
+                ButtonGridArea4.BringIntoView();
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                //buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (pName == "buttonGrid5")
+            {
+                ButtonGridArea5.BringIntoView();
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                //buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (pName == "buttonGrid6")
+            {
+                ButtonGridArea6.BringIntoView();
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                //buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+            else if (pName == "buttonGrid7")
+            {
+                ButtonGridArea7.BringIntoView();
+                buttonGrid1.ClearValue(UIElement.OpacityProperty);
+                buttonGrid2.ClearValue(UIElement.OpacityProperty);
+                buttonGrid3.ClearValue(UIElement.OpacityProperty);
+                buttonGrid4.ClearValue(UIElement.OpacityProperty);
+                buttonGrid5.ClearValue(UIElement.OpacityProperty);
+                buttonGrid6.ClearValue(UIElement.OpacityProperty);
+                //buttonGrid7.ClearValue(UIElement.OpacityProperty);
+            }
+        }
+
+        private void Canvas_Drop(object sender, DragEventArgs e)
+        {
+            
+            object data=e.Data.GetData(DataFormats.Serializable);
+            if(data is UIElement element)
+            {
+                Point DropPosition = e.GetPosition(AcceptDrop);
+                Canvas.SetLeft(element, DropPosition.X);
+                Canvas.SetTop(element, DropPosition.Y);
+                Canvas.SetRight(element, DropPosition.X);
+                Canvas.SetBottom(element, DropPosition.Y);
+                AcceptDrop.Children.Add(element);
+            }
+        }
+
+        private void Button_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+
+        }
+
+        private void AcceptDrop_DragEnter(object sender, DragEventArgs e)
+        {
+            object data = e.Data.GetData(DataFormats.Serializable);
+        }
+
+        private void AcceptDrop_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            object data = e.Data.GetData(DataFormats.Serializable);
+        }
+
+        private void AcceptDrop_DragLeave(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void AcceptDrop_PreviewDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void MoveButton_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+
+        }
+
+        private void AcceptDrop_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+
         }
     }
 }
